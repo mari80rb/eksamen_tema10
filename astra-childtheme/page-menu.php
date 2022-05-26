312a9f8a40cb8f5e029ba3dc1a00f569ec1b97c5
Jasmin Ersan <yase0236@stud.kea.dk>
2022-05-26 13:27:40 +0200

    php menu

---
+++ astra-childtheme/page-menu.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all single posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+
+
+<?php get_footer(); ?>

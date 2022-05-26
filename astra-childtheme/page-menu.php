8d9c189708bbb35722457f4978a13dd13ce82437
Mariyah Mahmood <mari80rb@stud.kea.dk>
2022-05-26 18:16:33 +0200

    JavaScript

--- astra-childtheme/page-menu.php
+++ astra-childtheme/page-menu.php
@@ -53,41 +53,89 @@ get_header();
 	</main>
 
 
-<script>
+	<script>
 
-	let drink;
+		let drink;
 				
-	const liste = document.querySelector("#drink-oversigt");
-	const skabelon = document.querySelector("template");
-	let filterDrink = "alle";
-	document.addEventListener("DOMContentLoaded", start);
+		const liste = document.querySelector("#drink-oversigt");
+		const skabelon = document.querySelector("template");
+		let filterDrink = "alle";
+		document.addEventListener("DOMContentLoaded", start);
 
-	function start() {
-		console.log("start");
-		getJson();
-	}
+		function start() {
+			console.log("start");
+			getJson();
+		}
 
-	const url = "http://mariyahmahmood.dk/kea/10_eksamen/teatime/wp-json/wp/v2/drink?per_page=100"
-	let kategorier;
-	let filter = "alle";
+		const url = "http://mariyahmahmood.dk/kea/10_eksamen/teatime/wp-json/wp/v2/drink?per_page=100"
+		const caturl = "http://mariyahmahmood.dk/kea/10_eksamen/teatime/wp-json/wp/v2/categories?per_page=100"
+		let kategorier;
+		let filter = "alle";
 
-	async function getJson() {
-	console.log("getJson");
-	let response = await fetch(url); 
-	const catdata = await fetch(caturl);
+		async function getJson() {
+			console.log("getJson");
+			let response = await fetch(url); 
+			const catdata = await fetch(caturl);
 
-	drink = await response.json();
-	kategorier = await catdata.json();
-	console.log(kategorier);
-	visDrinks(filter);
-	opretknapper();
-	addEventListenersToButtons();
+			drink = await response.json();
+			kategorier = await catdata.json();
+			console.log(kategorier);
+			visDrinks(filter);
+			opretknapper();
+			addEventListenersToButtons();
+		}	
+		
+		function addEventListenersToButtons() {
 
+			document.querySelectorAll("#knapper img").forEach(elm => {
+				elm.addEventListener("click", filtrerDrinks);
+			})
+		}
+
+		function opretknapper(){
 	
-} 
+			kategorier.forEach( function (kg){
+				/* Ved ik helt hvad der skal stå i vores ift unesco's, har prøvet at skrive det jeg tror */
+				document.querySelector("#knapper").innerHTML += `<img class="filter" data-drink="${kg.id}" name="${kg.navn}" src="${kg.ikoner}"></img>`; 
+	
+			})
+
+				addEventListenersToButtons();
+			}
+			
+		function visDrinks(filter) {
+
+			liste.innerHTML = "";
+			projekt.forEach(elm => {
+	
+				console.log(elm.categories.includes(parseInt(filter)));
+				if (elm.categories.includes(parseInt(filter)) || filter == "alle") {
+				console.log("foreach kører på drinks");
+				const klon = skabelon.cloneNode(true).content;
+				klon.querySelector("h2").textContent = elm.title.rendered; /*Hvad er title i vores? Det må være i deres WP backend */
+				klon.querySelector("img").src = elm.billede.guid; /*Hvad er billede/guid i vores? Det må være i deres WP backend*/
+
+				klon.querySelector("article").addEventListener("click", () => {location.href = elm.link;
+				})
+
+				liste.appendChild(klon);
+			}})
+		}
+		
+		function filtrerDrinks() {
+			filter = this.dataset.projekt;
+			document.querySelector(".kategorititel").textContent = this.getAttribute("navn");
+			projekt.forEach(elm => {
+				console.log(elm.categories);
+			})
+
+			visDrinks(filter);
+		}
 
 
-</script>
+			
+			
+	</script>
 
 
 </div>

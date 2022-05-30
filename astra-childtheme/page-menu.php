14261472f87ea2e7edf265c72a9e21e408f4cee2
Mariyah Mahmood <mari80rb@stud.kea.dk>
2022-05-30 14:38:41 +0200

    filtrering

--- astra-childtheme/page-menu.php
+++ astra-childtheme/page-menu.php
@@ -20,7 +20,38 @@ get_header();
 
 <style>
 
+	#drink-oversigt {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		grid-gap: 30px;
+		
+	
+		
+	}
 
+	#drink-oversigt img {
+		padding: 10px;
+		object-fit: cover;
+
+
+	
+	}
+
+	#knapper{
+
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+		grid-gap: 20px;
+		max-width: 1200px;
+		margin-inline: auto;
+		padding-bottom: 50px;
+	}
+
+	#knapper img{
+		object-fit: cover;
+		max-width: 100px;
+		align: center;
+	}
 
 </style>
 
@@ -71,6 +102,7 @@ get_header();
 
 		const url = "http://mariyahmahmood.dk/kea/10_eksamen/teatime/wp-json/wp/v2/drink?per_page=100"
 		const caturl = "http://mariyahmahmood.dk/kea/10_eksamen/teatime/wp-json/wp/v2/ikoner?per_page=100"
+		
 		let kategorier;
 		let filter = "alle";
 
@@ -81,10 +113,11 @@ get_header();
 			
 
 			drinks = await response.json();
+			console.log(drinks)
 			kategorier = await catdata.json();
 			console.log(kategorier);
 			visDrinks(filter);
-			opretknapper();
+			opretKnapper();
 			addEventListenersToButtons();
 		}	
 		
@@ -95,11 +128,11 @@ get_header();
 			})
 		}
 
-		function opretknapper(){
+		function opretKnapper(){
 	
 			kategorier.forEach( function (kg){
-				/* Ved ik helt hvad der skal stå i vores ift unesco's, har prøvet at skrive det jeg tror */
-				document.querySelector("#knapper").innerHTML += `<img class="filter" data-drink="${kg.id}" name="${kg.name}" src="${kg.ikon.guid}"></img>`; 
+				
+				document.querySelector("#knapper").innerHTML += `<img class="filter" data-drinks="${kg.id}" name="${kg.name}" src="${kg.ikon.guid}"></img>`; 
 	
 			})
 
@@ -109,14 +142,14 @@ get_header();
 		function visDrinks(filter) {
 
 			liste.innerHTML = "";
-			kategorier.forEach(elm => {
+			drinks.forEach(elm => {
 	
 				console.log(elm.ikoner.includes(parseInt(filter)));
 				if (elm.ikoner.includes(parseInt(filter)) || filter == "alle") {
 				console.log("foreach kører på drinks");
 				const klon = skabelon.cloneNode(true).content;
-				klon.querySelector("h2").textContent = elm.name.rendered; /*Hvad er title i vores? Det må være i deres WP backend */
-				klon.querySelector("img").src = elm.billede.guid; /*Hvad er billede/guid i vores? Det må være i deres WP backend*/
+				klon.querySelector("h2").textContent = elm.title.rendered; /*Hvad er title i vores? Det må være i deres WP backend */
+				klon.querySelector("img").src = elm.billede.guid; 
 
 				klon.querySelector("article").addEventListener("click", () => {location.href = elm.link;
 				})
